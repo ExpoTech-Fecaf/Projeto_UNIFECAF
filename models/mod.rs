ed68235ce196e950037a68cc78d87b2eba383c2a
Icaro Rodrigues <icaro.santosdev@gmail.com>
2026-05-07 11:00:36 -0300

    fix: removendo pastas duplicadas

--- models/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod user;
-pub mod product;
-pub mod role;
-pub use user::*;
-pub use product::*;
-pub use role::*;
--- services/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod auth_service;
-pub mod product_service;
-pub mod stock_service;
-pub mod user_service;
-pub mod role_service;
